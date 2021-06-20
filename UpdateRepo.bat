9586e31388b4a388c93b476a517b146bea1b5f8e
PlayerU99 <67335788+PlayerU99@users.noreply.github.com>
2021-06-20 19:38:10 +0530

    Oracle 18C Express Edition & tnsnames.ora error solved

--- UpdateRepo.bat
+++ UpdateRepo.bat
@@ -2,6 +2,6 @@ cd "D:\5.STUDY MATERIALS\LOGS"
 git --version
 git status
 git add .
-git commit -m "TOMCAT & JENKINS"
+git commit -m "Oracle 18C Express Edition & tnsnames.ora error solved"
 git push
 PAUSE
