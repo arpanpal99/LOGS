dff10f8084d157b5a52dc575613f3a0f4f19f55d
PlayerU99 <67335788+PlayerU99@users.noreply.github.com>
2021-06-27 15:54:22 +0530

    Unbound Classpath Container error - Importing a different JRE in Eclipse

--- UpdateRepo.bat
+++ UpdateRepo.bat
@@ -2,6 +2,6 @@ cd "D:\5.STUDY MATERIALS\LOGS"
 git --version
 git status
 git add .
-git commit -m "Linux Mint 20.1 Ulyssa"
+git commit -m "Unbound Classpath Container error - Importing a different JRE in Eclipse"
 git push
 PAUSE
