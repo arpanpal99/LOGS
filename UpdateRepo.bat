8d18d24af60142c19076ec3435a5deaf21875ba9
PlayerU99 <67335788+PlayerU99@users.noreply.github.com>
2021-06-27 13:48:28 +0530

    Linux Mint 20.1 Ulyssa

--- UpdateRepo.bat
+++ UpdateRepo.bat
@@ -2,6 +2,6 @@ cd "D:\5.STUDY MATERIALS\LOGS"
 git --version
 git status
 git add .
-git commit -m "JDBC - Connecting Oracle DB 18C from Eclipse"
+git commit -m "Linux Mint 20.1 Ulyssa"
 git push
 PAUSE
